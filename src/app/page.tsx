998b56804a8c710931a7417a0f3f4fb0aca77a02
Tarutyyyne <s1f102500405@iniad.org>
2025-05-01 18:17:10 +0900

    'puttableSearchとdisplayPuttableCellを純粋関数化'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,77 @@
 import { useState } from 'react';
 import styles from './page.module.css';
 
+//そのx,y座標に次のターンの色の石を置けるかを判定する関数
+const puttableSearch = (
+  board: number[][],
+  newBoard: number[][],
+  directions: number[][],
+  x: number,
+  y: number,
+  turnColor: number,
+) => {
+  //このx,yはそのマスのx,y座標
+  //そこに候補地が表示されていたら一度クリーンする
+  if (newBoard[y][x] === 3) {
+    console.log('a');
+    newBoard[y][x] = 0;
+  }
+  //そこに白黒があったらreturnして終了
+  if (newBoard[y][x] === 1 || newBoard[y][x] === 2) {
+    return;
+  }
+  //そこが空白なら置けるかどうかの探索を開始する
+  for (const [dy, dx] of directions) {
+    const nextY: number = y + dy;
+    const nextX: number = x + dx;
+    //隣の石が次のターンの色と異なる色（現在の色）をしているときに石をおける
+    if (newBoard[nextY] !== undefined && newBoard[nextY][nextX] === turnColor) {
+      //初期値+1に方向のベクトルをj倍かけてそこに空白がないか検索
+      for (let j = 1; j < newBoard.length; j++) {
+        const extendSearchY: number = dy * j;
+        const extendSearchX: number = dx * j;
+        if (
+          newBoard[nextY + extendSearchY] !== undefined &&
+          newBoard[nextY + extendSearchY][nextX + extendSearchX] % 3 === 0
+        ) {
+          break; //そこに空白と候補地があれば石を置けないのでfor文を終わらせる
+        }
+        if (
+          //行きついた先に次のターンの色の石があったら
+          newBoard[nextY + extendSearchY] !== undefined &&
+          newBoard[nextY + extendSearchY][nextX + extendSearchX] === 2 / turnColor
+        ) {
+          newBoard[y][x] = 3; //候補地をおく
+        }
+      }
+    }
+  }
+};
+
+//for文で64マスそれぞれにputtableSearch()を実行して候補地を表示する関数
+const displayPuttableCell = (
+  board: number[][],
+  newBoard: number[][],
+  directions: number[][],
+  turnColor: number,
+) => {
+  for (let p = 0; p < board.flat().length; p++) {
+    const coordinateX = p % 8;
+    const coordinateY = Math.floor(p / 8);
+    puttableSearch(board, newBoard, directions, coordinateX, coordinateY, turnColor);
+    console.log(board[coordinateY][coordinateX]);
+  }
+};
+
 export default function Home() {
   const [turnColor, setTurnColor] = useState(1);
   const [board, setBoard] = useState([
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 3, 0, 0, 0],
+    [0, 0, 0, 1, 2, 3, 0, 0],
+    [0, 0, 3, 2, 1, 0, 0, 0],
+    [0, 0, 0, 3, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
   ]);
@@ -26,65 +88,13 @@ export default function Home() {
     [-1, -1], //左上
   ];
   const newBoard = structuredClone(board); //以下structureClone()というboardの配列を変更する関数
-  const flatBoard: number[] = board.flat();
 
   //二次元配列のboardをflat()で一次元にならし、filter()で条件にあう要素だけを並べた新しい配列をつくり、その配列の長さを取得する
   const blackAmount: number = board.flat().filter((board) => board === 1).length;
   const whiteAmount: number = board.flat().filter((board) => board === 2).length;
   const redAmount: number = board.flat().filter((board) => board === 3).length;
-  //countPassをここで定義
-  // console.log('check through');
-  // let countPass: number = 0;
-  // console.log('check countPass');
-
-  //そのx,y座標に次のターンの色の石を置けるかを判定する関数
-  const puttableSearch = (x: number, y: number) => {
-    //このx,yはそのマスのx,y座標
-    //そこに候補地が表示されていたら一度クリーンする
-    if (newBoard[y][x] === 3) {
-      console.log('delete 3');
-      newBoard[y][x] = 0;
-    }
-    //そこに白黒があったらreturnして終了
-    if (newBoard[y][x] === 1 || newBoard[y][x] === 2) {
-      return;
-    }
-    //そこが空白なら置けるかどうかの探索を開始する
-    for (const [dy, dx] of directions) {
-      const nextY: number = y + dy;
-      const nextX: number = x + dx;
-      //隣の石が次のターンの色と異なる色（現在の色）をしているときに石をおける
-      if (newBoard[nextY] !== undefined && newBoard[nextY][nextX] === turnColor) {
-        //初期値+1に方向のベクトルをj倍かけてそこに空白がないか検索
-        for (let j = 1; j < board.length; j++) {
-          const extendSearchY: number = dy * j;
-          const extendSearchX: number = dx * j;
-          if (
-            newBoard[nextY + extendSearchY] !== undefined &&
-            newBoard[nextY + extendSearchY][nextX + extendSearchX] % 3 === 0
-          ) {
-            break; //そこに空白と候補地があれば石を置けないのでfor文を終わらせる
-          }
-          if (
-            //行きついた先に次のターンの色の石があったら
-            newBoard[nextY + extendSearchY] !== undefined &&
-            newBoard[nextY + extendSearchY][nextX + extendSearchX] === 2 / turnColor
-          ) {
-            newBoard[y][x] = 3; //候補地をおく
-          }
-        }
-      }
-    }
-  };
-  //for文で64マスそれぞれにputtableSearch()を実行して候補地を表示する関数
-  const displayPuttableCell = () => {
-    //要修正丸パクリいや
-    for (let p = 0; p < flatBoard.length; p++) {
-      const coordinateX = p % 8;
-      const coordinateY = Math.floor(p / 8);
-      puttableSearch(coordinateX, coordinateY);
-    }
-  };
+  //countPassをここで定義;
+  const [countPass, setCountPass] = useState(0);
 
   const clickHandler = (x: number, y: number) => {
     //クリックして変化する動作はすべてこの関数の中に記述する
@@ -96,17 +106,19 @@ export default function Home() {
     }
     //候補地の表示がなくなったときにパスを実行
 
-    console.log(countPass);
     if (redAmount === 0) {
-      // if (countPass === 0 || countPass === 1) {
-      setTurnColor(2 / turnColor);
-      displayPuttableCell();
-      // countPass += 1;
-      // console.log(countPass);
-      console.log('Pass Your Turn');
-      // } else {
-      // console.log('Game Over');
-      // }
+      if (countPass === 0) {
+        setTurnColor(2 / turnColor);
+        displayPuttableCell(board, newBoard, directions, turnColor);
+        setCountPass(1);
+        console.log(countPass);
+        console.log('Pass Your Turn');
+      }
+      if (countPass === 1) {
+        if (redAmount === 0) {
+          console.log('Game Over');
+        }
+      }
     }
     //クリックしたところは空白ではないので関数を実行
     //クリックしたら八方向にむける
@@ -136,13 +148,11 @@ export default function Home() {
             for (let k = 0; k < j + 1; k++) {
               newBoard[nextY + directions[i][0] * k][nextX + directions[i][1] * k] = turnColor;
             }
+            displayPuttableCell(board, newBoard, directions, turnColor);
             setTurnColor(2 / turnColor);
-            displayPuttableCell();
             setBoard(newBoard);
             console.log('reset countPass');
-            console.log(countPass);
-            countPass = 0;
-            console.log(countPass);
+            setCountPass(0);
 
             break; //石を置いたのにまだ続けるわけにはいかないから
           }
